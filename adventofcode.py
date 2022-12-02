43c9b463bb7df9efc7d7e758b0c12562b8a3da08
T.J. Eckman <jeek@jeek.net>
2022-12-02 15:00:15 -0500

    Comment

--- adventofcode.py
+++ adventofcode.py
@@ -37,6 +37,7 @@ def problem02d(inputfile="02.input", part=1):
     return sum([*map(lambda line:[*map(lambda x:ord(x)%47,'OZJ6XYEBP7]\=RDK3[AS')].index(part*ord(line[0])*ord(line[2])%47)//2,open(inputfile).read().splitlines())])
 
 def problem02e(inputfile="02.input", part=1):
+    """Problem #2, alternate solution."""
     return sum(1+("BXCXAXAYBYCYCZAZBZBXCYAZAXBYCZCXAYBZ".find(line[0]+line[2],(part%2)*18)%18)//2 for line in open(inputfile).read().split("\n"))
 
 TESTDATA = [
