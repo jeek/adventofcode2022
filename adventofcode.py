02c2b5e22a3135495f50c840141623c63de12ba1
T.J. Eckman <jeek@jeek.net>
2022-12-02 04:17:51 -0500

    Problem #2

--- adventofcode.py
+++ adventofcode.py
@@ -3,7 +3,7 @@
 import unittest
 from collections import defaultdict
 from copy import copy
-from itertools import product
+from itertools import product, permutations
 from heapq import heappop, heappush
 
 def problem01(inputfile="01.input", part=1):
@@ -13,13 +13,26 @@ def problem01(inputfile="01.input", part=1):
 
 def problem02(inputfile="02.input", part=1):
     """Problem #2."""
-    with open(inputfile) as file:
-        data = [int(i) for i in file.read().split("\n")]
-        return data[0]
+    return sum([*map(lambda line:[*map(lambda x:ord(x)%47,'OZJ6XYEBP7]\=RDK3[AS')].index(part*ord(line[0])*ord(line[2])%47)//2,open(inputfile).read().splitlines())])
+    #return sum([69,420,27,7,41,42,22,19,33,8,46,45,14,35,21,28,4,44,18,36].index(part*ord(line[0])*ord(line[2])%47)//2 for line in open(inputfile).read().split("\n"))
+    #return sum([[9,1,5,6,7,2,3,4,8],[9,1,5,7,2,6,8,3,4]][part-1][(ord(line[0])%3)*3+ord(line[2])%3] for line in open(inputfile).read().split("\n"))
+    #return sum([["B X","C Y","A Z","A X","B Y","C Z","C X","A Y","B Z"],["B X","C X","A X","A Y","B Y","C Y","C Z","A Z","B Z"]][part-1].index(line)+1 for line in open(inputfile).read().split("\n"))
+    # Below is my old solution before I one-linered it.
+    if False:
+        score = 0
+        for line in [i for i in open(inputfile).read().split("\n")]:
+            them, me = line.split(" ")
+            oldme = me
+            if part == 2:
+                me = ["ZXY","XYZ","YZX"]["XYZ".index(me)]["ABC".index(them)]
+            oldscore = score
+            score += " XYZ".index(me) + (6 if "ABC".index(them)=="YZX".index(me) else 3 if "ABC".index(them)=="XYZ".index(me) else 0)
+            print(part, score-oldscore, them, oldme)
+        return score
 
 TESTDATA = [
     ["Problem_01", problem01, 1, 24000, 45000, 68802, 205370],
-    ["Problem_02", problem02, 2, 0, 0, 0, 0]
+    ["Problem_02", problem02, 2, 15, 12, 11150, 8295]
 ]
 
 class TestSequence(unittest.TestCase):
