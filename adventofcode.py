b0a3fe942221c4ed35ad12e805eaa218a3ec7a18
T.J. Eckman <jeek@jeek.net>
2022-12-02 14:31:52 -0500

    Decided to test all the solutions

--- adventofcode.py
+++ adventofcode.py
@@ -12,23 +12,37 @@ def problem01(inputfile="01.input", part=1):
 
 def problem02(inputfile="02.input", part=1):
     """Problem #2."""
+    score = 0
+    for line in [i for i in open(inputfile).read().split("\n")]:
+        them, me = line.split(" ")
+        if part == 2:
+            me = ["ZXY","XYZ","YZX"]["XYZ".index(me)]["ABC".index(them)]
+        score += " XYZ".index(me) + (6 if "ABC".index(them)=="YZX".index(me) else 3 if "ABC".index(them)=="XYZ".index(me) else 0)
+    return score
+
+def problem02a(inputfile="02.input", part=1):
+    """Problem #2, alternate solution."""
     return sum([*map(lambda line:[*map(lambda x:ord(x)%47,'OZJ6XYEBP7]\=RDK3[AS')].index(part*ord(line[0])*ord(line[2])%47)//2,open(inputfile).read().splitlines())])
-    #return sum([69,420,27,7,41,42,22,19,33,8,46,45,14,35,21,28,4,44,18,36].index(part*ord(line[0])*ord(line[2])%47)//2 for line in open(inputfile).read().split("\n"))
-    #return sum([[9,1,5,6,7,2,3,4,8],[9,1,5,7,2,6,8,3,4]][part-1][(ord(line[0])%3)*3+ord(line[2])%3] for line in open(inputfile).read().split("\n"))
-    #return sum([["B X","C Y","A Z","A X","B Y","C Z","C X","A Y","B Z"],["B X","C X","A X","A Y","B Y","C Y","C Z","A Z","B Z"]][part-1].index(line)+1 for line in open(inputfile).read().split("\n"))
-    # Below is my old solution before I one-linered it.
-    if False:
-        score = 0
-        for line in [i for i in open(inputfile).read().split("\n")]:
-            them, me = line.split(" ")
-            if part == 2:
-                me = ["ZXY","XYZ","YZX"]["XYZ".index(me)]["ABC".index(them)]
-            score += " XYZ".index(me) + (6 if "ABC".index(them)=="YZX".index(me) else 3 if "ABC".index(them)=="XYZ".index(me) else 0)
-        return score
+
+def problem02b(inputfile="02.input", part=1):
+    """Problem #2, alternate solution."""
+    return sum([69,420,27,7,41,42,22,19,33,8,46,45,14,35,21,28,4,44,18,36].index(part*ord(line[0])*ord(line[2])%47)//2 for line in open(inputfile).read().split("\n"))
+
+def problem02c(inputfile="02.input", part=1):
+    """Problem #2, alternate solution."""
+    return sum([[9,1,5,6,7,2,3,4,8],[9,1,5,7,2,6,8,3,4]][part-1][(ord(line[0])%3)*3+ord(line[2])%3] for line in open(inputfile).read().split("\n"))
+
+def problem02d(inputfile="02.input", part=1):
+    """Problem #2, alternate solution."""
+    return sum([["B X","C Y","A Z","A X","B Y","C Z","C X","A Y","B Z"],["B X","C X","A X","A Y","B Y","C Y","C Z","A Z","B Z"]][part-1].index(line)+1 for line in open(inputfile).read().split("\n"))
 
 TESTDATA = [
     ["Problem_01", problem01, 1, 24000, 45000, 68802, 205370],
-    ["Problem_02", problem02, 2, 15, 12, 11150, 8295]
+    ["Problem_02", problem02, 2, 15, 12, 11150, 8295],
+    ["Problem_02a", problem02a, 2, 15, 12, 11150, 8295],
+    ["Problem_02b", problem02b, 2, 15, 12, 11150, 8295],
+    ["Problem_02c", problem02c, 2, 15, 12, 11150, 8295],
+    ["Problem_02d", problem02d, 2, 15, 12, 11150, 8295],
 ]
 
 class TestSequence(unittest.TestCase):
